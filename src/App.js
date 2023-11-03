e4c6f408449e2a4e21dd6152160f4780166079b2
Pasha <majorovpaul@gmail.com>
2023-11-03 18:09:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import Dropdown from "react-dropdown";
+import { HiSwitchHorizontal } from "react-icons/hi";
+import "react-dropdown/style.css";
+import "./App.css";
 
 function App() {
+  // Переменные
+  const [info, setInfo] = useState([]);
+  const [input, setInput] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [options, setOptions] = useState([]);
+  const [output, setOutput] = useState(0);
+
+  //АПИ
+  useEffect(() => {
+    Axios.get(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`
+    ).then((res) => {
+      setInfo(res.data[from]);
+    });
+  }, [from]);
+
+  // Вызов функции
+
+  useEffect(() => {
+    setOptions(Object.keys(info));
+    convert();
+  }, [info]);
+
+  function convert() {
+    var rate = info[to];
+    setOutput(input * rate);
+  }
+
+  function flip() {
+    var temp = from;
+    setFrom(to);
+    setTo(temp);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="heading">
+        <h1>Конвертер валют</h1>
+      </div>
+      <div className="container">
+        <div className="left">
+          <h3>Ввод</h3>
+          <input
+            type="text"
+            placeholder="Введите число"
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+        <div className="middle">
+          <h3>Из</h3>
+          <Dropdown
+            options={options}
+            onChange={(e) => {
+              setFrom(e.value);
+            }}
+            value={from}
+            placeholder="From"
+          />
+        </div>
+        <div className="switch">
+          <HiSwitchHorizontal
+            size="30px"
+            onClick={() => {
+              flip();
+            }}
+          />
+        </div>
+        <div className="right">
+          <h3>В</h3>
+          <Dropdown
+            options={options}
+            onChange={(e) => {
+              setTo(e.value);
+            }}
+            value={to}
+            placeholder="To"
+          />
+        </div>
+      </div>
+      <div className="result">
+        <button
+          onClick={() => {
+            convert();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Конвертировать
+        </button>
+        <h2>Результат:</h2>
+        <p>{input + " " + from + " = " + output.toFixed(2) + " " + to}</p>
+      </div>
     </div>
   );
 }
